c7d35645ac5c2c657af7286a1baae2658a88f261
armdupre <a-joseph.dupre@keysight.com>
2023-05-16 16:36:19 -0400

    Upated.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-cloudist-azure-1-vnet-1-public-subnet
 Terraform module for Vnet deployment on Microsoft Azure
+
+This module creates a deployment topology with a single virtual network having a single public facing subnet.
--- output.tf
+++ output.tf
@@ -1,16 +1,19 @@
 output "PublicNetworkSecurityGroup" {
+	description = "Network security group resource associated with the public subnet"
 	value = {
 		"id" : azurerm_network_security_group.PublicNetworkSecurityGroup.id
 	}
 }
 
 output "PublicSubnet" {
+	description = "Subnet resource associated with the public subnet"
 	value = {
 		"id" : azurerm_subnet.PublicSubnet.id
 	}
 }
 
 output "Vnet" {
+	description = "Vnet resource associated with the virtual network"
 	value = {
 		"id" : azurerm_virtual_network.Vnet.id
 	}
