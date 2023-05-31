5b8b284d3202a7c249231b30930c52530600692a
armdupre <a-joseph.dupre@keysight.com>
2023-05-31 19:56:41 -0400

    Updated.

--- locals.tf
+++ locals.tf
@@ -1,20 +1,19 @@
 locals {
-	AppTag = var.AppTag
-	PrivateNetworkSecurityGroupName = "${local.UserLoginTag}-${local.AppTag}-private-network-security-group"
-	PrivateSubnetName = "${local.UserLoginTag}-${local.AppTag}-vnet-private-subnet"
-	PrivateSubnetPrefix = var.PrivateSubnetPrefix
-	PublicHttpSecurityRuleName = "${local.UserLoginTag}-${local.AppTag}-web-rule"
-	PublicHttpsSecurityRuleName = "${local.UserLoginTag}-${local.AppTag}-https-rule"
-	PublicNetworkSecurityGroupName = "${local.UserLoginTag}-${local.AppTag}-network-security-group"
+	InstanceId = var.InstanceId
+	Preamble = "${local.UserLoginTag}-${local.Tag}-${local.InstanceId}"
+	PublicHttpSecurityRuleName = "${local.Preamble}-http-rule"
+	PublicHttpsSecurityRuleName = "${local.Preamble}-https-rule"
+	PublicNetworkSecurityGroupName = "${local.Preamble}-public-nsg"
 	PublicSecurityRuleSourceIpPrefix = var.PublicSecurityRuleSourceIpPrefix
-	PublicSshSecurityRuleName = "${local.UserLoginTag}-${local.AppTag}-ssh-rule"
-	PublicSubnetName = "${local.UserLoginTag}-${local.AppTag}-vnet-public-subnet"
+	PublicSshSecurityRuleName = "${local.Preamble}-ssh-rule"
+	PublicSubnetName = "${local.Preamble}-public-subnet"
 	PublicSubnetPrefix = var.PublicSubnetPrefix
 	ResourceGroupLocation = var.ResourceGroupLocation
 	ResourceGroupName = var.ResourceGroupName
+	Tag = var.Tag
 	UserEmailTag = var.UserEmailTag
 	UserLoginTag = var.UserLoginTag
 	UserProjectTag = var.UserProjectTag
 	VnetAddressPrefix = var.VnetAddressPrefix
-	VnetName = "${local.UserLoginTag}-${local.AppTag}-vnet"
+	VnetName = "${local.Preamble}-vnet"
 }
